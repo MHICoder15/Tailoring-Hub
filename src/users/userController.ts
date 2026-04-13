b260f0194fa62f5388c6b7bed417f65e69ed9721
Hasnain Malik <hasnain.i@dplit.com>
2026-04-13 23:18:26 +0500

    Add password hash

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,6 +1,7 @@
 import type { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel.ts";
+import bcrypt from "bcrypt";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   // Validation
@@ -15,6 +16,9 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(409, "User with this email already exists");
     return next(error);
   }
+  // Hashing password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  
   // Processing
   // Response
   res.json({ message: "User registered successfully" });
