7045a43726950b7ccc6f128b7ecb19546fc0a330
Hasnain Malik <hasnain.i@dplit.com>
2026-04-13 22:19:24 +0500

    Add Global Error Handler

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from "express";
+import globleErrorHandler from "./middlewares/globle-error-handler.js";
+import createHttpError from "http-errors";
 
 const app = express();
 
 // Routes
 // HTTP methods: GET, POST, PUT, PATCH, DELETE
 app.get("/", (req, res) => {
+  const error = createHttpError(400, "Something went wrong");
+  throw error;
   res.json({ message: "Welcome to Tailoring Hub API" });
 });
+
+// Global error handler
+app.use(globleErrorHandler);
 
 export default app;
