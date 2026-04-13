e2e650a7b57c558033a8a05af15033231b787fd6
Hasnain Malik <hasnain.i@dplit.com>
2026-04-13 22:56:55 +0500

    Add User Register Validation

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,8 +1,17 @@
 import type { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
 const createUser = (req: Request, res: Response, next: NextFunction) => {
+  // Validation
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
+
+  // Processing
+  // Response
   res.json({ message: "User registered successfully" });
-  next();
 };
 
 export { createUser };
