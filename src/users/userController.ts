58151c0cad1c4f023facc0a2f3493e0a8bcd9340
Hasnain Malik <mhicoder@gmail.com>
2026-04-20 22:33:34 +0500

    Add Error Handling

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -4,6 +4,7 @@ import userModel from "./userModel.ts";
 import bcrypt from "bcrypt";
 import Jwt from "jsonwebtoken";
 import { config } from "../config/config.ts";
+import type { User } from "./userTypes.ts";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   // Validation
@@ -13,18 +14,38 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(error);
   }
   // Database operations
-  const user = await userModel.findOne({ email: email });
-  if (user) {
-    const error = createHttpError(409, "User with this email already exists");
-    return next(error);
+  try {
+    const user = await userModel.findOne({ email: email });
+    if (user) {
+      const error = createHttpError(409, "User with this email already exists");
+      return next(error);
+    }
+  } catch (error) {
+    console.error(error);
+    const httpError = createHttpError(500, "Error occurred while getting user");
+    return next(httpError);
   }
   // Hashing password
   const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = await userModel.create({ name, email, password: hashedPassword });
+  let newUser: User;
+  try {
+    newUser = await userModel.create({ name, email, password: hashedPassword });
+  } catch (error) {
+    console.error(error);
+    const httpError = createHttpError(500, "Error occurred while creating user");
+    return next(httpError);
+  }
   // Token generation JWT
-  const token = Jwt.sign({ id: newUser._id }, config.jwtSecret as string, { expiresIn: "1d" });
-  // Response
-  res.json({ message: "User registered successfully", id: newUser._id, accessToken: token });
+  try {
+    const token = Jwt.sign({ id: newUser._id }, config.jwtSecret as string, { expiresIn: "1d" });
+    // Response
+    res.json({ message: "User registered successfully", id: newUser._id, accessToken: token });
+
+  } catch (error) {
+    console.error(error);
+    const httpError = createHttpError(500, "Error occurred while signing JWT token");
+    return next(httpError);
+  }
 };
 
 export { createUser };
