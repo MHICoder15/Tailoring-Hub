54769ee13ca0c67640dad9fcfca17467d7ef97e6
Hasnain Malik <mhicoder@gmail.com>
2026-04-20 22:55:10 +0500

    Add Login Endpoint

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -48,4 +48,49 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export { createUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  // Validation
+  const { email, password } = req.body;
+  if (!email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
+  // Database operations
+  let user: User | null;
+  try {
+    user = await userModel.findOne({ email: email });
+    if (!user) {
+      const error = createHttpError(401, "Invalid email or password");
+      return next(error);
+    }
+  } catch (error) {
+    console.error(error);
+    const httpError = createHttpError(500, "Error occurred while getting user");
+    return next(httpError);
+  }
+  // Comparing password
+  try {
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      const error = createHttpError(401, "Invalid email or password");
+      return next(error);
+    }
+  } catch (error) {
+    console.error(error);
+    const httpError = createHttpError(500, "Error occurred while comparing passwords");
+    return next(httpError);
+  }
+  // Token generation JWT
+  try {
+    const token = Jwt.sign({ id: user._id }, config.jwtSecret as string, { expiresIn: "1d" });
+    // Response
+    res.status(200).json({ message: "User logged in successfully", id: user._id, accessToken: token });
+
+  } catch (error) {
+    console.error(error);
+    const httpError = createHttpError(500, "Error occurred while signing JWT token");
+    return next(httpError);
+  }
+};
+
+export { createUser, loginUser };
