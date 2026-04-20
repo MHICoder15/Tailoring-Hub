3ec815d1bb3573d7eca4ac01db7bd2a39a52dbbd
Hasnain Malik <mhicoder@gmail.com>
2026-04-20 20:11:58 +0500

    Sign JWT Token

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -2,6 +2,8 @@ import type { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel.ts";
 import bcrypt from "bcrypt";
+import Jwt from "jsonwebtoken";
+import { config } from "../config/config.ts";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   // Validation
@@ -12,16 +14,17 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
   // Database operations
   const user = await userModel.findOne({ email: email });
-  if(user) {
+  if (user) {
     const error = createHttpError(409, "User with this email already exists");
     return next(error);
   }
   // Hashing password
   const hashedPassword = await bcrypt.hash(password, 10);
-  
-  // Processing
+  const newUser = await userModel.create({ name, email, password: hashedPassword });
+  // Token generation JWT
+  const token = Jwt.sign({ id: newUser._id }, config.jwtSecret as string, { expiresIn: "1d" });
   // Response
-  res.json({ message: "User registered successfully" });
+  res.json({ message: "User registered successfully", id: newUser._id, accessToken: token });
 };
 
 export { createUser };
